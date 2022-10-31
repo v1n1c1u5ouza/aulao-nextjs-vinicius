19202754fb9523bca38e082bf3b49eb6b792fc3c
v1n1c1us5ouza <viniciusdesouzamarinho.com>
2022-10-31 11:54:22 -0300

    I learned how to use module.css

---
+++ fundamentos/src/pages/stylish.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Stylish.module.css'
+
+export default function Stylish() {
+    return (
+        <div className={styles.roxo}>
+            <h1>Estilo usando CSS Módulo</h1>
+        </div>
+    )
+}
+
+
