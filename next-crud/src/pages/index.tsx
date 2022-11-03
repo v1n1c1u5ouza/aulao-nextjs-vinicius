b0713057b221bf2311ce6bc8a629b2a4535e08b6
v1n1c1us5ouza <viniciusdesouzamarinho.com>
2022-11-03 10:51:18 -0300

    Add: Typescript and my own layout

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,10 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={``}>
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-900 to-purple-900
+      text-white
+    `}>
+      <Layout title="Cadastro Simples">
+        <span>Conteudo</span>
+      </Layout>
     </div>
   )
 }
