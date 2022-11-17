b8b34e8f708f4d6baf98f4eb1bda0707dfe25429
v1n1c1us5ouza <viniciusdesouzamarinho.com>
2022-11-17 11:05:10 -0300

    Add: Interval component

--- redux-simples/src/App.jsx
+++ redux-simples/src/App.jsx
@@ -1,16 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Card from "./components/Card";
+import Interval from "./components/Interval";
 
 function App() {
   return (
     <div className="App">
       <h1>Exercios React-Redux (Simples)</h1>
       <div className="linha">
-        <Card title="Card 1" red>X</Card>
-        <Card title="Card 2" green>X</Card>
+        <Interval></Interval>
       </div>
       <div className="linha">
+        <Card title="Card 2" green>X</Card>
         <Card title="Card 3" blue>Y</Card>
         <Card title="Card 4" purble>Y</Card>
       </div>
---
+++ redux-simples/src/components/Interval.jsx
@@ -0,0 +1,21 @@
+import "./Interval.css";
+import React from "react";
+
+import Card from "./Card";
+
+export default (props) => {
+  return (
+    <Card title="Intevalo de Números" red>
+      <div className="Interval">
+        <span>
+          <strong>Mínimo:</strong>
+          <input type="number" value={0} readOnly />
+        </span>
+        <span>
+          <strong>Máximo:</strong>
+          <input type="number" value={10} readOnly />
+        </span>
+      </div>
+    </Card>
+  );
+};
