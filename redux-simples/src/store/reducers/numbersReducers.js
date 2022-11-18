3db947167fd59953fa2568f57e6ca4c85686dd24
v1n1c1us5ouza <viniciusdesouzamarinho.com>
2022-11-18 09:36:57 -0300

    Refactoring reducers

---
+++ redux-simples/src/store/reducers/numbersReducers.js
@@ -0,0 +1,21 @@
+const initalState = {
+  min: 1,
+  max: 10,
+};
+
+export default function (state = initalState, action) {
+  switch (action.type) {
+    case "NUM_MIN_ALTERED":
+      return {
+        ...state,
+        min: action.payload,
+      };
+    case "NUM_MAX_ALTERED":
+      return {
+        ...state,
+        max: action.payload,
+      };
+    default:
+      return state;
+  }
+}
--- redux-simples/src/store/storeConfig.js
+++ redux-simples/src/store/storeConfig.js
@@ -1,29 +1,9 @@
 import { createStore, combineReducers } from "redux";
 
+import numbersReducers from "./reducers/numbersReducers";
+
 const reducers = combineReducers({
-  numbers: function (state, action) {
-    switch (action.type) {
-      case "NUM_MIN_ALTERED":
-        return {
-          ...state,
-          min: action.payload,
-        };
-      case "NUM_MAX_ALTERED":
-        return {
-          ...state,
-          max: action.payload,
-        };
-      default:
-        return {
-          min: 0,
-          max: 5,
-        };
-    }
-  },
-  names: function (state, action) {
-    console.log(state, "", action);
-    return ["Ana", "Bia", "Carlos"];
-  },
+  numbers: numbersReducers,
 });
 
 function storeConfig() {
