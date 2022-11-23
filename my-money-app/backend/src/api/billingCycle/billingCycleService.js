3f8151f67140516d42472a1d50ed113a8bb112b6
v1n1c1us5ouza <viniciusdesouzamarinho.com>
2022-11-23 08:36:34 -0300

    Fix: Get returning a empty array

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -3,4 +3,14 @@ const BillingCycle = require("./billingCycle");
 BillingCycle.method(["get", "post", "put", "delete"]);
 BillingCycle.updateOptions({ new: true, runValidatos: true });
 
+BillingCycle.route("get", (req, res, next) => {
+  BillingCycle.find({}, (err, docs) => {
+    if (!err) {
+      res.json(docs);
+    } else {
+      res.status(500).json({ errors: [error] });
+    }
+  });
+});
+
 module.exports = BillingCycle;
