5162884d56c7ab325fc4dbe43dac2cf928917000
v1n1c1us5ouza <viniciusdesouzamarinho.com>
2022-11-24 12:38:40 -0300

    Add: footer to the page

--- my-money-app/frontend/src/main/app.jsx
+++ my-money-app/frontend/src/main/app.jsx
@@ -1,11 +1,17 @@
 import "../common/templetes/dependencies";
 import React from "react";
+
 import Header from "../common/templetes/header";
 import SideBar from "../common/templetes/sideBar";
+import Footer from "../common/templetes/footer";
 
 export default (props) => (
   <div className="wrapper">
     <Header />
     <SideBar />
+    <div className="content-wrapper">
+      <h1>Conteudo</h1>
+    </div>
+    <Footer />
   </div>
 );
