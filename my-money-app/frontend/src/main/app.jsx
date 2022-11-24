4d9a4925936a84b08e1eefa842bf1048326db98b
v1n1c1us5ouza <viniciusdesouzamarinho.com>
2022-11-24 10:40:34 -0300

    Add: Header component

--- my-money-app/frontend/src/main/app.jsx
+++ my-money-app/frontend/src/main/app.jsx
@@ -1,8 +1,9 @@
 import "../common/templetes/dependencies";
 import React from "react";
+import Header from "../common/templetes/header";
 
 export default (props) => (
   <div className="wrapper">
-    <h1>app</h1>
+    <Header /> 
   </div>
 );
