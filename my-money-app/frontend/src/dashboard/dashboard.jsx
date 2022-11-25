c15a24d75a75d31e9bf38aec2b92c0bce2e9172b
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-11-25 09:22:41 -0300

    Feat: Header component

--- my-money-app/frontend/src/dashboard/dashboard.jsx
+++ my-money-app/frontend/src/dashboard/dashboard.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default props => (
-    <div>
-        <h1>Dashboard</h1>
-    </div>
-)
+import ContenttHeader from "../common/templetes/contentHeader";
+import Content from "../common/templetes/content";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <ContenttHeader title="Dashboard" small="Versão 1.0" />
+        <Content>Dashboard</Content>
+      </div>
+    );
+  }
+}
+
+export default Dashboard
