b98ff15191d6869549c256f0c1598ae6e718149c
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-11-25 08:46:23 -0300

    Add: Routes to dashboard and billingcycles page

--- my-money-app/frontend/src/main/app.jsx
+++ my-money-app/frontend/src/main/app.jsx
@@ -4,13 +4,14 @@ import React from "react";
 import Header from "../common/templetes/header";
 import SideBar from "../common/templetes/sideBar";
 import Footer from "../common/templetes/footer";
+import Routes from "./routes";
 
 export default (props) => (
   <div className="wrapper">
     <Header />
     <SideBar />
     <div className="content-wrapper">
-      <h1>Conteudo</h1>
+      <Routes />
     </div>
     <Footer />
   </div>
