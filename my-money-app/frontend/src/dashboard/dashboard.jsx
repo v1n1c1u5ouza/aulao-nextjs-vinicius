f062ee30f478671f6d035f921116f737c2b8a99e
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-11-25 10:53:22 -0300

    Feat: Grid and Value box components to dashboard page

--- my-money-app/frontend/src/dashboard/dashboard.jsx
+++ my-money-app/frontend/src/dashboard/dashboard.jsx
@@ -2,16 +2,42 @@ import React, { Component } from "react";
 
 import ContenttHeader from "../common/templetes/contentHeader";
 import Content from "../common/templetes/content";
+import ValueBox from "../common/widget/valueBox";
+import Row from "../common/layout/row";
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <ContenttHeader title="Dashboard" small="Versão 1.0" />
-        <Content>Dashboard</Content>
+        <Content>
+          <Row>
+            <ValueBox
+              cols="12 4"
+              color="green"
+              icon="bank"
+              value="R$ 10"
+              text="Total de Créditos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="red"
+              icon="credit-card"
+              value="R$ 10"
+              text="Total de Débitos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="blue"
+              icon="monezy"
+              value="R$ 0"
+              text="Valor Consilidado"
+            />
+          </Row>
+        </Content>
       </div>
     );
   }
 }
 
-export default Dashboard
+export default Dashboard;
